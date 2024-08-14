e7be39b853041c174b285bc20f24a05ce7877fa4
rick915 alves <henriquelago2017@gmail.com>
2024-08-14 20:51:23 +0000

    começo js

---
+++ script.js
@@ -0,0 +1,13 @@
+const respostaAPI = [
+    {"id": 1, "descricao": "varrer a sala"},
+    {"id": 2, "descricao": "revisar a aula de bandeira"},
+    {"id": 3, "descricao": "lavar louça"}
+];
+
+function init(){
+    carregar_lista();
+}
+
+function carregar_lista(){
+    
+}
